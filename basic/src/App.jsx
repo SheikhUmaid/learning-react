783eb5bac1b924a74c7f32ecfd8765c16a282cde
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2024-08-08 13:12:42 +0530

    Hooks

--- basic/src/App.jsx
+++ basic/src/App.jsx
@@ -1,9 +1,34 @@
+import {useState} from "react";
+import "./App.css"
+
+
 function App() {
+
+  let [counter, setCounter] = useState(Number.MAX_VALUE)
+
+
+  // let counter = 0;
+  const add = ()=>{
+    setCounter(counter + (Number.MAX_VALUE / 2));
+  }
+
+  const sub = ()=>{
+    setCounter(counter - (Number.MAX_VALUE / 2));
+  }
+
+
+
   return (
-    <h1>
-    tea and code with vite helo world
-    </h1>
+    <>
+    <div className="Hello">
+      <h1>React Counter app</h1>
+      <button onClick={add}><h1>+</h1></button>
+      <h3>Counter value: {counter}</h3>
+      <button onClick={sub}><h1>-</h1></button>
+    </div>
+    </>
   )
 }
+
 
 export default App
